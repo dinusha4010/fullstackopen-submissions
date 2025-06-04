3daa9fa6f0e5d735f6a24d46d75a37ab4f495593
dinusha4010 <dinusha4010@gmail.com>
2025-06-04 07:32:12 +0000

    1.10 done

--- part1/unicafe/introdemo/src/App.jsx
+++ part1/unicafe/introdemo/src/App.jsx
@@ -1,33 +1,72 @@
 import { useState } from 'react'
 
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+const Statistics = (props) => {
+  if (props.total > 0) {
+    return (
+      <>
+        <h1>statistics</h1>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={props.good} />
+            <StatisticLine text="neutral" value={props.neutral} />
+            <StatisticLine text="bad" value={props.bad} />
+            <StatisticLine text="all" value={props.total} />
+            <StatisticLine text="average" value={props.average} />
+            <StatisticLine text="positive" value={`${props.positive.toFixed(1)} %`} />
+          </tbody>
+        </table>
+      </>
+    )
+  }
+  return (
+    <>
+      <h1>statistics</h1>
+      <p>No feedback given</p>
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const total=good+neutral+bad
-  const average=(good*1+neutral*0+bad*-1)/total
-  const positive=good/total
+  const total = good + neutral + bad
+  const average = (good * 1 + neutral * 0 + bad * -1) / total
+  const positive = (good / total) * 100
+
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>
-        good
-      </button>
-      <button onClick={() => setNeutral(neutral + 1)}>
-        neutral
-      </button>
-      <button onClick={() => setBad(bad + 1)}>
-        bad
-      </button>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {average}</p>
-      <p>positive {positive.toFixed(14)} %</p>
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <Statistics 
+        good={good} 
+        neutral={neutral} 
+        bad={bad} 
+        total={total} 
+        average={average} 
+        positive={positive} 
+      />
     </div>
   )
 }
